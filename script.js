7ecc74389a551122e16963cc0ecb3ffc5fc6c2a4
Gustavo Lopes Lorenz <gustavolorenz43@gmail.com>
2022-05-22 18:08:26 -0300

    adicionando missil do invader,particulas e colisoes, nao necessariamente nessa mesma ordem

--- script.js
+++ script.js
@@ -89,6 +89,56 @@ class Projectile{
     }
 
 }
+class Particle{
+    constructor({position, velocity, radius, color}){
+        this.position = position
+        this.velocity = velocity
+        this.radius = radius
+        this.color = color
+        this.opacity = 1
+    }
+    
+    draw(){
+        c.save()
+        c.globalAlpha = this.opacity
+        c.beginPath()
+        c.arc(this.position.x, this.position.y,this.radius,0, Math.PI * 2)
+        c.fillStyle = this.color
+        c.fill()
+        c.closePath()
+        c.restore()
+    }
+    
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        this.opacity -= 0.01
+    }
+
+}
+class InvaderProjectile{
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+        this.width = 3
+        this.height = 10
+    }
+    // aqui eu crio o missil do invader (mas ainda nao esta desenhado)
+    draw(){
+        c.fillStyle = 'white'
+        c.fillRect(this.position.x, this.position.y,this.width,this.height)
+
+    }
+    //e aqui sim eu desenho o missil
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+
+}
+
 class Invader{
     
     constructor({position}){
@@ -136,7 +186,18 @@ class Invader{
         }
 
     }
-   
+    shoot(invaderProjectiles){
+        invaderProjectiles.push(new InvaderProjectile({
+            position:{
+                x: this.position.x + this.width / 2,
+                y:this.position.y + this.height
+            },
+            velocity:{
+                x: 0,
+                y: 5
+            }
+        }))
+    }
 }
 
 class Grid{
@@ -178,6 +239,8 @@ class Grid{
 const player = new Player()
 const projectiles = []
 const grids = []
+const invaderProjectiles = []
+const particles = []
 
 const keys ={
     a:{
@@ -196,7 +259,23 @@ let frames = 0
 //o let randomInterval é para ter um controle da primeira grid de inimigos.
 let randomInterval = Math.floor((Math.random() * 500) + 500)
 
-
+function createParticles({object,color}){
+    for(let i = 0 ; i < 15; i += 1){
+        particles.push(new Particle({
+            position:{
+                x: object.position.x + object.width / 2,
+                 y: object.position.y + object.height /2
+            },
+            velocity:{
+                x: (Math.random() - 0.5) * 2,
+                y: (Math.random() - 0.5) * 2
+            },
+            radius: Math.random() * 3,
+            color: color || '#BAA0DE' 
+        })
+    )
+    }
+}
 //func feita para gerenciar a animação do jogo,atualizando prejeteis,jogador e inimigo
 function animate(){
     requestAnimationFrame(animate)
@@ -206,6 +285,38 @@ function animate(){
     c.fillRect(0,0, canvas.width,canvas.height)
     
     player.update()
+    particles.forEach((particle, i) => {
+        if(particle.opacity <= 0 ){
+            setTimeout(() => {
+                particle.splice(i, 1)
+
+            }, 0)
+        }else{
+            particle.update()
+        }
+        
+    })
+    invaderProjectiles.forEach((invaderProjectile, index) =>{
+        if (invaderProjectile.position.y + invaderProjectile.height >= canvas.height){
+            setTimeout(() => {
+                invaderProjectiles.splice(index, 1)
+            },0)
+        }else invaderProjectile.update()
+        //missil atingindo player
+        if(invaderProjectile.position.y + invaderProjectile.height >= player.position.y && invaderProjectile.position.x + invaderProjectile.width >= player.position.x && invaderProjectile.position.x <= player.position.x + player.width){
+            
+            
+            setTimeout(() => {
+                invaderProjectiles.splice(index, 1)
+            },0)
+            console.log("yoyu lose")
+            createParticles({
+                object: player,
+                color:  'white'
+            })
+        }
+        
+    })
     projectiles.forEach((projectile, index) =>{
         //nesse if eu faço o controle para quando  os projeteis sairem da tela  eu deleto eles
         if(projectile.position.y + projectile.radius <= 0){
@@ -218,13 +329,20 @@ function animate(){
         }
         //projectile.update()
     })
-    grids.forEach(grid =>{
+    grids.forEach((grid, gridIndex) =>{
         grid.update()
+            //spawn inimigos projectiles
+    if(frames % 100 === 0 && grid.invaders.length > 0){
+        grid.invaders[Math.floor(Math.random() * grid.invaders.length)].shoot(invaderProjectiles)
+    }
+
+
         grid.invaders.forEach((invader, i)=>{
             invader.update({velocity: grid.velocity})
             projectiles.forEach((projectile, j) =>{
                 //aqui é aonde eu faço a colisao do missil com o inimigo e confiro todos os lados da interação do inimigo com o missil
                 if(projectile.position.y - projectile.radius <= invader.position.y + invader.height && projectile.position.x + projectile.radius >= invader.position.x && projectile.position.x - projectile.radius <= invader.position.x + invader.width && projectile.position.y + projectile.radius >= invader.position.y){
+
                     setTimeout(()=>{
                         //aqui pelo que entendi , esse codigo é feito porque toda vez que eu removo algum objeto do array com o metodo splice o array se arranja
                         //causando um comportamento "erroneo" dos inimigos a serem removidos do arrray cada vez que o missil detecta uma colisao
@@ -232,35 +350,51 @@ function animate(){
                             
                         
                         const projectileFound = projectiles.find((projectile2) => projectile2 === projectile)
+                        //e aqui enfim eles sao de fato removidos.
                         if(invaderFound && projectileFound){
+                            
+                            createParticles({
+                                object: invader
+                            })
+
                             grid.invaders.splice(i,1)
                             projectiles.splice(j, 1)
-                        }    
+                            if (grid.invaders.length > 0){
+                            const firstInvader = grid.invaders[0]
+                            const lastInvader = grid.invaders[grid.invaders.length - 1]
+                            grid.width = lastInvader.position.x - firstInvader.position.x + lastInvader.width
+                            grid.position.x = firstInvader.position.x
+                            }else{
+                                grids.splice(gridIndex,1)
+                            }
+                        }
                     }, 0)
                 }
 
             })
         })
     })
-    if (keys.a.pressed && player.position.x >= 0){
-        player.velocity.x = -5
-        player.rotation = -.15
-    }else if(keys.d.pressed && player.position.x + player.width <= canvas.width){
-        player.velocity.x = +5
-        player.rotation = 0.15
-
-    }
-    else{
-        player.velocity.x = 0
-        player.rotation = 0
-    }
+        if (keys.a.pressed && player.position.x >= 0){
+            player.velocity.x = -5
+            player.rotation = -.15
+            console.log("AAAAAAAA")
+        }else if(keys.d.pressed && player.position.x + player.width <= canvas.width){
+            player.velocity.x = +5
+            player.rotation = 0.15
+            console.log("DDDDDDDDD")
+        }
+        else{
+            player.velocity.x = 0
+            player.rotation = 0
+        }
     //aqui é aonde eu controlo o spawns das grids de inimigos o frames é meu contador que é resetado sempre que uma grid eh criada.
     if(frames % randomInterval === 0){
         grids.push(new Grid())
         randomInterval = Math.floor((Math.random() * 500) + 500)
         frames = 0
-        
     }
+
+
     frames++
 }
 
@@ -271,13 +405,15 @@ addEventListener('keydown', ({key}) => {
             
             
             keys.a.pressed = true
+            //console.log("AAAAAAAA!!!!!!!!!!!!!!!")
             break
         case 'd':
             
             keys.d.pressed = true
+            //console.log("DDDDDDDDD!!!!!!!!!!!")
             break
         case ' ':
-            
+            //console.log("olha abala")
             projectiles.push(new Projectile({
                 position:{
                     x: player.position.x + player.width / 2,
@@ -299,13 +435,15 @@ addEventListener('keyup', ({key}) => {
             
             
             keys.a.pressed = false
+            //console.log("NAOOOOOO")
             break
         case 'd':
             
             keys.d.pressed = false
+            //console.log("PERDIIIIII")
             break
         case ' ':
-            
+            //console.log("MAS AQUI VAI")
             break
     }   
 }
